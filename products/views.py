1e9aed26849edc2454c4ecaa04b77a6cb7890d9f
Telesia <sarahtelesia@gmail.com>
2021-10-20 18:37:30 +0000

    Add category cards

--- products/views.py
+++ products/views.py
@@ -3,9 +3,11 @@ from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
 
+# Create your views here.
+
 
 def all_products(request):
-    """ A view to return all products, including searching and sorting """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
@@ -20,13 +22,14 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+          
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -35,7 +38,7 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter a search criteria")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
@@ -54,7 +57,7 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """ A view to show individual products """
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
